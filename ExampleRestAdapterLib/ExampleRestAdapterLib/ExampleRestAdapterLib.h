4978b3f496ebd45810de7877237842f0abe1550d
Dave Welch <dave.welch@mokinetworks.com>
2013-10-22 11:18:32 -0600

    Initial commit

---
+++ ExampleRestAdapterLib/ExampleRestAdapterLib/ExampleRestAdapterLib.h
@@ -0,0 +1,13 @@
+//
+//  ExampleRestAdapterLib.h
+//  ExampleRestAdapterLib
+//
+//  Created by Dave Welch on 10/22/13.
+//  Copyright (c) 2013 David Welch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ExampleRestAdapterLib : NSObject
+
+@end
---
+++ ExampleRestAdapterLib/ExampleRestAdapterLib/ExampleRestAdapterLib.m
@@ -0,0 +1,13 @@
+//
+//  ExampleRestAdapterLib.m
+//  ExampleRestAdapterLib
+//
+//  Created by Dave Welch on 10/22/13.
+//  Copyright (c) 2013 David Welch. All rights reserved.
+//
+
+#import "ExampleRestAdapterLib.h"
+
+@implementation ExampleRestAdapterLib
+
+@end
